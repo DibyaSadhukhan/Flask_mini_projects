520e37ea75098b9dd32dff78ec1db9ebdf8d8796
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2021-10-29 02:30:06 +0530

    frontend fix and database connect

--- 1.To-Do-list/app.py
+++ 1.To-Do-list/app.py
@@ -1,16 +1,28 @@
 import flask
+import flask_sqlalchemy
 app = flask.Flask(__name__, template_folder="Templates")
-@app.route('/')
-@app.route('/home')
-def home():
-     return flask.render_template('index.html')
-@app.route('/forms',methods=['POST'])
-def forms():
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db= flask_sqlalchemy.SQLAlchemy(app)
+
+class item(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    task=db.Column(db.String(), nullable=False,unique=True)
+    Time=db.Column(db.String())
+    active=db.Column(db.Integer())
+    def __repr__(self):
+        return (f'Item {self.task}')
+
+
+@app.route('/',methods=['POST','GET'])
+@app.route('/home',methods=['POST','GET'])
+def home(): 
     if flask.request.method == 'POST':
-        f_name=flask.request.form['fname']
-        l_name=flask.request.form['lname']
-        return (flask.render_template('form_post.html',f_name=f_name,l_name=l_name))
+        next_todo=flask.request.form['title']
+        print(next_todo)
+        list=item.query.all()
+        return (flask.render_template('index.html',list=list))
     else:
-        return flask.render_template('index.html')
+        list=item.query.all()
+        return (flask.render_template('index.html',list=list))
 if __name__=='__main__':
     app.run(debug=True)
