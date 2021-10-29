8e8fcdfea9ebe9e6b0cbb0b03e2c201c8a00b4e5
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2021-10-29 22:47:28 +0530

    docs

---
+++ 1.To-Do-list/Readme.md
@@ -0,0 +1,6 @@
+This project is a to do list 
+total creation time : 7-8 hrs
+how to run it locally:
+    fork this folder navigate to this folder in your command prompt 
+    type in
+    python app.py
--- 1.To-Do-list/app.py
+++ 1.To-Do-list/app.py
@@ -4,8 +4,9 @@ from datetime import datetime
 app = flask.Flask(__name__, template_folder="Templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db= flask_sqlalchemy.SQLAlchemy(app)
-
+#creating database model
 class item(db.Model):
+    #our database has the task the time stamp and the active as columns
     id=db.Column(db.Integer(),primary_key=True)
     task=db.Column(db.String(), nullable=False,unique=True)
     Time=db.Column(db.String())
@@ -13,7 +14,7 @@ class item(db.Model):
     def __repr__(self):
         return (f'Item {self.task}')
 
-
+# we can add two decorators for the same wheel ie. both '/' and '/home' will take me to the same page
 @app.route('/',methods=['POST','GET'])
 @app.route('/home',methods=['POST','GET'])
 def home(): 
